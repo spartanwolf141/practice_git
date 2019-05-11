95aa7d7f574cfd4323ba86367b5a5209fc6c0a63
spartanwolf141 <49828012+spartanwolf141@users.noreply.github.com>
2019-05-10 20:47:22 -1000

    added functions and variable day

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
+var day = 'Friday';
+console.log(day)
 
+function fullName(firstname, lastname){
+    return firstname + ' '+ lastname
+  }
+  var myName = fullName('Steven', 'Stone');
+  console.log(myName)
+  
+  var num1 = 3;
+  var num2 = 9;
+  
+  function add(num1, num2){
+    return num1+num2
+  }
+  console.log(add(num1, num2))
+  
+  function sen(num1, num2){
+    var sum = num1 + num2;
+    return num1 + ' plus ' + num2 + ' equals ' + sum; 
+  }
+  console.log(sen(num1, num2))
